704b6ab4227d50f1a0b91cd0e19ad16616a7e891
Umberto Raimondi <me@uraimo.com>
2018-06-05 21:30:25 +0200

    SwiftyGPIO to 1.x

--- Examples/Raspi2/Package.swift
+++ Examples/Raspi2/Package.swift
@@ -2,9 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "TestDS18B20",
-    targets: [],
     dependencies: [
         .Package(url: "https://github.com/uraimo/DS18B20.swift.git",
-                 majorVersion: 1)
+                 majorVersion: 2)
     ]
 )
