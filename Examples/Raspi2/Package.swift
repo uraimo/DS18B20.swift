42c060726aa089130a49659c78aa97709b3cee4e
Umberto Raimondi <me@uraimo.com>
2019-09-26 18:34:57 +0200

    Now requiring Swift 4.x+

--- Examples/Raspi2/Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "TestDS18B20",
-    dependencies: [
-        .Package(url: "https://github.com/uraimo/DS18B20.swift.git",
-                 majorVersion: 2)
-    ]
-)
