9e48bcd8615ad18e012ff40f9e497cccdf4e796d
Umberto Raimondi <me@uraimo.com>
2019-09-26 18:34:57 +0200

    Now requiring Swift 4.x+

--- Package.swift
+++ Package.swift
@@ -1,9 +1,22 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "DS18B20",
+    products: [
+        .library(
+            name: "DS18B20",
+            targets: ["DS18B20"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git",
-                 majorVersion: 1)
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "DS18B20",
+            dependencies: ["SwiftyGPIO"],
+            path: ".",
+            sources: ["Sources"])
     ]
 )
